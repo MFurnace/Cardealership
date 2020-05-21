145f5e579c24ca0dd78483faf5c3bd6dab2b48a1
MFurnace <mfurnace1@gamil.com>
2020-05-21 08:11:16 -0400

    update

---
+++ Leaseable.java
@@ -0,0 +1,9 @@
+interface Leaseable {
+   //Intreface normally follow a naming convention similar 2 classes
+   // but they end in -'ible' or 'able' 
+   //usually adjective becasue we interface to provide additional capabilities
+
+boolean isLeaseable();
+int getLeaseTerm();
+int getMilesPerYear();
+}
